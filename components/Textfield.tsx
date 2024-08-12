a3177da114eadc87e746e59e0b24e2f75009e549
Alexs Ruiz <ralexs.acu@gmail.com>
2024-08-12 17:05:36 -0600

    Add a new reusable component Textfield

---
+++ components/Textfield.tsx
@@ -0,0 +1,13 @@
+import type { SizeTokens } from 'tamagui';
+import { XStack, Input, Button } from 'tamagui';
+
+const Textfield = (props: { size: SizeTokens }) => {
+  return (
+    <XStack alignItems="center" space="$2">
+      <Input flex={1} size={props.size} placeholder={`Busca un area ...`} />
+      <Button size={props.size}>Go</Button>
+    </XStack>
+  );
+};
+
+export default Textfield;
