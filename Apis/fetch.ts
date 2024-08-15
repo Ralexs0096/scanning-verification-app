a066b18941a76c16f25618408e130e022dd412c0
Alexs Ruiz <ralexs.acu@gmail.com>
2024-08-14 22:22:51 -0600

    Define a fetch helper fn

---
+++ Apis/fetch.ts
@@ -0,0 +1,18 @@
+const baseURL = process.env.EXPO_PUBLIC_API_URL;
+
+type HTTPMethods = 'GET' | 'POST';
+
+type FetchBase = { endpoint: string; data?: any; method?: HTTPMethods };
+
+export const fetchBase = ({
+  endpoint,
+  data,
+  method = 'GET'
+}: FetchBase): Promise<Response> =>
+  fetch(`${baseURL}/${endpoint}`, {
+    method,
+    headers: {
+      'Content-type': 'application/json'
+    }
+    // body: JSON.stringify(data)
+  });
