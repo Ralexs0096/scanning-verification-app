3822e59d80556c36987c0b0c90c17f19309eaf67
Alexs Ruiz <ralexs.acu@gmail.com>
2024-08-14 22:23:21 -0600

    Correct Wording

--- components/Textfield.tsx
+++ components/Textfield.tsx
@@ -5,7 +5,7 @@ const Textfield = (props: { size: SizeTokens }) => {
   return (
     <XStack alignItems="center" space="$2">
       <Input flex={1} size={props.size} placeholder={`Busca un area ...`} />
-      <Button size={props.size}>Go</Button>
+      <Button size={props.size}>Buscar</Button>
     </XStack>
   );
 };
