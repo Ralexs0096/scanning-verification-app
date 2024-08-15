7e8ccd6f90f9b1345f5303e1c4be8f92d402e55a
Alexs Ruiz <ralexs.acu@gmail.com>
2024-08-14 23:04:50 -0600

    Display the name of the Area and code

--- app/[area].tsx
+++ app/[area].tsx
@@ -1,11 +1,17 @@
 import { Text } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useLocalSearchParams } from 'expo-router';
+import { Paragraph } from 'tamagui';
 
 export default () => {
+  const { area, name } = useLocalSearchParams();
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <Text>Area Details</Text>
+      <Paragraph theme="alt1">
+        Area: {name} ({area})
+      </Paragraph>
     </SafeAreaView>
   );
 };
