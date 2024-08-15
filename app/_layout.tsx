8788f058a6ca77b2ac8adec4f4a4f29933e87a2a
Alexs Ruiz <ralexs.acu@gmail.com>
2024-08-14 22:51:30 -0600

    Load the custom font required for tamagui
    - reference:  https://tamagui.dev/docs/guides/expo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,9 @@
 import { config } from '@tamagui/config/v3';
 import { createTamagui, TamaguiProvider } from '@tamagui/core';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useFonts } from 'expo-font';
 import { Slot } from 'expo-router';
+import { useEffect } from 'react';
 
 const tamaguiConfig = createTamagui(config);
 
@@ -14,6 +16,21 @@ declare module '@tamagui/core' {
 const queryClient = new QueryClient();
 
 export default () => {
+  const [loaded] = useFonts({
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf')
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      // can hide splash screen here
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <TamaguiProvider config={tamaguiConfig}>
       <QueryClientProvider client={queryClient}>
