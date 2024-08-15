3a53dadd3e819f1c9b506838c2a870963278301b
Alexs Ruiz <ralexs.acu@gmail.com>
2024-08-14 22:20:30 -0600

    Define the Area Response Type

---
+++ Apis/types.ts
@@ -0,0 +1,14 @@
+type AreasResponse = {
+  codigo_are: string;
+  descripcion_are: string;
+  codigo_mod: number;
+  codigo_cos: string;
+  ince_pro: string;
+  codigo_ant: string;
+  no_transporte: string;
+  fecha_mod: Date;
+  costura: string;
+  produ: string;
+  estado: boolean;
+  depto: number;
+};
