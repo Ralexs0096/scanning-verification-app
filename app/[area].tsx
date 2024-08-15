25f23137cc6bc838bb2d0df09ecbb55234729bfb
Alexs Ruiz <ralexs.acu@gmail.com>
2024-08-14 22:54:56 -0600

    Prevent notch collision on /area route

--- app/[area].tsx
+++ app/[area].tsx
@@ -1,10 +1,11 @@
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
 import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default () => {
   return (
-    <View>
+    <SafeAreaView style={{ flex: 1 }}>
       <Text>Area Details</Text>
-    </View>
+    </SafeAreaView>
   );
 };
