a6b9c4b3125e3bf922ec32895f1a3f1720e2b8da
Alexs Ruiz <ralexs.acu@gmail.com>
2024-08-24 00:59:35 -0600

    Add a TODO for area search input

--- app/index.tsx
+++ app/index.tsx
@@ -10,6 +10,7 @@ export default () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <YStack minHeight={80} overflow="hidden" margin="$3" padding="$2">
+        // TODO: implement the search by area
         <Textfield size={32} />
       </YStack>
 
