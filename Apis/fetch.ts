87bcbb62b50f2eb810bf295247dc5da574c6fb43
Alexs Ruiz <ralexs.acu@gmail.com>
2024-08-24 00:47:31 -0600

    Adjust the fetchBase function to inject the data on the body correctly

--- Apis/fetch.ts
+++ Apis/fetch.ts
@@ -8,11 +8,19 @@ export const fetchBase = ({
   endpoint,
   data,
   method = 'GET'
-}: FetchBase): Promise<Response> =>
-  fetch(`${baseURL}/${endpoint}`, {
+}: FetchBase): Promise<Response> => {
+  const headers: HeadersInit = {
+    'Content-type': 'application/json'
+  };
+
+  const config: RequestInit = {
     method,
-    headers: {
-      'Content-type': 'application/json'
-    },
-    ...(!!data && { body: JSON.stringify(data) })
-  });
+    headers
+  };
+
+  if (data && Object.keys(data).length > 0) {
+    config.body = JSON.stringify(data);
+  }
+
+  return fetch(`${baseURL}/${endpoint}`, config);
+};
